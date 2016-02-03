1b352402688cbcb98fed8dcc66cebb06a2c3af8e
DJCagle <jlc304@txstate.edu>
2016-02-03 00:07:07 -0600

    Added global dict 'plays' to store 'id' and play data. Updated play data.

--- A1.py
+++ A1.py
@@ -1,20 +1,26 @@
-import yaml
 import os
-import json
+import yaml
+import glob
 
 from flask import Flask
 from flask import render_template
 
-app = Flask(__name__)
-
-for fn in json.glob('data/*.yaml'):
+# create global plays dictionary
+plays = {}
+# loop over the files
+for fn in glob.glob('data/*.yaml'):
+    # load data from fn and put in dictionary
     with open(fn, 'r') as yf:
-        play = json.load(yf)
+        play = yaml.load(yf)
+        plays[play['id']] = play
+
+
+app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', plays=plays)
 
 
 if __name__ == '__main__':
