58bdee26c16348f86c7764f451acd98789ea0518
DJCagle <jlc304@txstate.edu>
2016-02-02 23:08:01 -0600

    Imported json and added import function for play data.

--- A1.py
+++ A1.py
@@ -1,11 +1,20 @@
+import yaml
+import os
+import json
+
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
+for fn in json.glob('data/*.yaml'):
+    with open(fn, 'r') as yf:
+        play = json.load(yf)
+
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
